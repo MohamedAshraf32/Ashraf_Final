ffbcf89ab9a13d9da576703d46fa9a86fc1a442c
Mohamed Ashraf Faragallah <masrhef@gmail.com>
2026-02-12 13:05:36 +0200

    Remove extra space in S3 bucket name

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket         = "ashrof-final-gp1 " # REPLACE THIS WITH YOUR ACTUAL BUCKET NAME
+    bucket         = "ashrof-final-gp1" # REPLACE THIS WITH YOUR ACTUAL BUCKET NAME
     key            = "eks-platform/terraform.tfstate"
     region         = "eu-west-1"
     encrypt        = true
